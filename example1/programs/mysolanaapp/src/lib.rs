cbd6db21a2e2107fb86c3d6ffa91ee9025d4c24e
legendary <legendary.phoenix.icarus@gmail.com>
2022-05-28 15:11:55 +0200

    deploy contract - example1

--- example1/programs/mysolanaapp/src/lib.rs
+++ example1/programs/mysolanaapp/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("56RF9ChDsQGY2aUuNs21hyhXU9ikQ1MjhJjnFXfNpwjK");
 
 #[program]
 mod mysolanaapp {
